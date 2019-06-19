55379b4b5150f5ab415e56532eef19d3598071ce
Claudiagari <claudia.garibotto@gmail.com>
2019-06-18 19:43:04 -0500

    subiendo el proyecto

---
+++ application/language/espanol/application_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['usuarios'] = "Usuarios";
+$lang['aliados'] = "Nuestros Aliados";
+$lang['banners'] = "Banners";
+$lang['estadisticas'] = "Estadísticas";
+$lang['precios'] = "Precios";
+$lang['testimonios'] = "Testimonios";
+$lang['campanias'] = "Campañas";
+$lang['programas'] = "Contáctanos";
+$lang['paises'] = "Países";
+$lang['formulario'] = "Formulario - Únete";
+$lang['pagos'] = "Registro de Pagos";
+
+/* End of file application_lang.php */
+/* Location: ./application/language/espanol/application_lang.php */
